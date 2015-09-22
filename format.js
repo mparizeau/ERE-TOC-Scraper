bab419cea47ea846753569704e22c6875edd9a08
Matt Parizeau <mparizeau@gmail.com>
2015-09-21 23:06:57 -0700

    reorganize functions into format file

--- format.js
+++ format.js
@@ -11,26 +11,60 @@ function getPostHTML(postData) {
 	});	
 }
 
-module.exports = function(postData) {
-	var groupedPosts = _.groupBy(postData, function(post) {
-		return moment(post.originallyPosted).format('MMMM YYYY');
-	});
+module.exports = {
+	parseHTML: function(body) {
+		var $ = cheerio.load(body);
+		var posts = [];
+		$('.post-meta').each(function(i, element) {
+			var $this = $(this);
+			// id is of the form post-XXXX
+			var postId = $this.attr('id').split('-')[1];
+			var titleLink = $this.find('h1 a');
+			var originallyPosted = $this.find('#bte_opp small').text();
+			var postDate;
+			if (originallyPosted === '') {
+				// if there isn't an originally posted date then the post hasn't been republished
+				// and we should look at the published date
+				var published = $this.find('.post-date').text();
+				var publishedDateString = published.match(/Published on (.+)/)[1];
+				postDate = moment(publishedDateString, 'MMMM Do YYYY').toDate();
+			} else {
+				var postDateString = originallyPosted.match(/Originally posted (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})/)[1];
+				postDate = new Date(postDateString);
+			}
 
-	var finalHTML = cheerio('<html>');
-	var bodyHTML = cheerio('<body>');
-	finalHTML.append(bodyHTML);
-
-	for (var month in groupedPosts) {
-		var monthContainer = cheerio('<p>');
-		monthContainer.append(cheerio('<strong>').text(month)).append('<br>');
-
-		var postHTML = getPostHTML(groupedPosts[month]);
-		postHTML.forEach(function(post) {
-			monthContainer.append(post).append('<br>');
+			posts.push({
+				title: titleLink.attr('title'),
+				link: titleLink.attr('href'),
+				originallyPosted: postDate,
+				id: postId
+			});
 		});
 
-		bodyHTML.append(monthContainer);
-	}
+		return posts;
+	},
 
-	return cheerio.html(finalHTML);
-}
+	getHTML: function(postData) {
+		var groupedPosts = _.groupBy(postData, function(post) {
+			return moment(post.originallyPosted).format('MMMM YYYY');
+		});
+
+		var finalHTML = cheerio('<html>');
+		var bodyHTML = cheerio('<body>');
+		finalHTML.append(bodyHTML);
+
+		for (var month in groupedPosts) {
+			var monthContainer = cheerio('<p>');
+			monthContainer.append(cheerio('<strong>').text(month)).append('<br>');
+
+			var postHTML = getPostHTML(groupedPosts[month]);
+			postHTML.forEach(function(post) {
+				monthContainer.append(post).append('<br>');
+			});
+
+			bodyHTML.append(monthContainer);
+		}
+
+		return cheerio.html(finalHTML);
+	}
+};
--- main.js
+++ main.js
@@ -1,46 +1,12 @@
 var fs = require('fs');
 var request = require('request');
-var cheerio = require('cheerio');
-var moment = require('moment');
 var async = require('async');
-var formatHTML = require('./format');
-
-function parseHTML(body) {
-	var $ = cheerio.load(body);
-	var posts = [];
-	$('.post-meta').each(function(i, element) {
-		var $this = $(this);
-		// id is of the form post-XXXX
-		var postId = $this.attr('id').split('-')[1];
-		var titleLink = $this.find('h1 a');
-		var originallyPosted = $this.find('#bte_opp small').text();
-		var postDate;
-		if (originallyPosted === '') {
-			// if there isn't an originally posted date then the post hasn't been republished
-			// and we should look at the published date
-			var published = $this.find('.post-date').text();
-			var publishedDateString = published.match(/Published on (.+)/)[1];
-			postDate = moment(publishedDateString, 'MMMM Do YYYY').toDate();
-		} else {
-			var postDateString = originallyPosted.match(/Originally posted (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})/)[1];
-			postDate = new Date(postDateString);
-		}
-
-		posts.push({
-			title: titleLink.attr('title'),
-			link: titleLink.attr('href'),
-			originallyPosted: postDate,
-			id: postId
-		});
-	});
-
-	return posts;
-}
+var format = require('./format');
 
 function requestPage(pageIndex, callback) {
 	request('http://earlyretirementextreme.com/page/' + pageIndex, function(error, response, body) {
 		if (!error && response.statusCode === 200) {
-			callback(null, parseHTML(body));
+			callback(null, format.parseHTML(body));
 		} else if (error) {
 			// if there's an error e.g. socket hang up then retry
 			console.log("retrying " + pageIndex);
@@ -94,7 +60,7 @@ async.until(
 		});
 		fs.writeFileSync('results.txt', JSON.stringify(finalPosts));
 
-		var html = formatHTML(finalPosts);
+		var html = format.getHTML(finalPosts);
 		fs.writeFileSync('results.html', html);
 	}
 );
